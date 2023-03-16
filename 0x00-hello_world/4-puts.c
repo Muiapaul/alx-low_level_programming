3af14d9cc6f250eabfe4d480a96fa2581d37960d
Muiapaul <muiapaul1997@gmail.com>
2023-03-16 16:33:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
  * main -print a line of code using puts
  *
- * return: 0
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

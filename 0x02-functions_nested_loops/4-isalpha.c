2c615ec54eafe38262bc8b607c76177193917da1
Muiapaul <muiapaul1997@gmail.com>
2023-03-21 06:39:55 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  * _isalpha -checks for alphabetic character.
  * @c: character to be checked
- * return: 1 for alphabetic character or 0 for anything else
+ *Return: Always 1 (Success) or 0 for anything else
 */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >=97 && c <=122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}

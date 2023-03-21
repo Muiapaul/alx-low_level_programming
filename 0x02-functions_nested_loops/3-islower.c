e4f1c646f561959d328e64ccc7c925f4b94832ec
Muiapaul <muiapaul1997@gmail.com>
2023-03-21 06:03:10 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower -checks for lowercase character
+ * Return: Always 1 (Success) for lowercase character or 0 for anything else
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}

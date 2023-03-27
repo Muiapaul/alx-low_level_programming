21292d157e03449dfd8f6905011d7803982ea797
Muiapaul <muiapaul1997@gmail.com>
2023-03-27 03:35:10 -0700

    new file:   0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "maim.h"
+/**
+ * reset_to_98 -takes a pointer to an int as parameter and updates the value
+ * @n: number inputed
+ * return: n always (success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
